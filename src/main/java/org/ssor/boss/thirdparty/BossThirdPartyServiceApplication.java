806a41ef947ba7bb6121082b40330e09e934fccc
Bermond Jon Batistiana <bermond.batistiana@smoothstack.com>
2021-06-12 14:57:55 -0700

    First Commit

---
+++ src/main/java/org/ssor/boss/thirdparty/BossThirdPartyServiceApplication.java
@@ -0,0 +1,15 @@
+package org.ssor.boss.thirdparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BossThirdPartyServiceApplication
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(BossThirdPartyServiceApplication.class, args);
+  }
+
+}
